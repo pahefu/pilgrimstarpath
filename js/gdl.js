d01c0bdbfe66398da6ab1bb6d3da2a19e0a1312c
Pablo H <pahefu@gmail.com>
2016-10-10 13:38:38 +0200

    Updating style. Use svg instead canvas.

--- js/gdl.js
+++ js/gdl.js
@@ -1,20 +1,27 @@
 
 var center = {
   coords: [2047,127,2047],
-  name: 'Galaxy Center'
+  name: 'Galaxy Center',
+  mapCoords : [0,0,0]
 };
 
 var pilgrim = {
   coords: [0x64a,0x0082,0x1b9],
-  name: 'Pilgrim Star'
+  name: 'Pilgrim Star',
+  mapCoords : [0,0,0]
 };
 
 var userLocation = {
   coords: [0xeeb,0x0081,0x91a],
   name: 'User Location',
-  enabled: false
+  enabled: false,
+  mapCoords : [0,0,0]
 }
 
+
+
+var wp = undefined;
+var hp = undefined;
 
 var canvasWidth = undefined;
 var canvasHeight = undefined;
@@ -23,145 +30,268 @@ var realCanvasHeight = undefined;
 var ctx = undefined;
 var legendPercent = undefined;
 
-function drawGrid(){
+
+var svgHelper = {
 	
-	ctx.strokeStyle = '#999999';
-	var step = 30;
+	svg : undefined,	
 	
-	for(var i = 0;i<canvasWidth;i+=step){
+	init: function(domId, a, b){
+		this.svg = document.getElementById(domId);
+		
+		if(a == undefined){			
+			var parent = document.getElementById("plotparent");
+			wp = parent.getBoundingClientRect().width * 0.9;
+			hp = wp * (document.documentElement.clientHeight*1.0/document.documentElement.clientWidth); 
+			
+		}else{
+			wp = a;
+			hp = b;
+		}
+		
+		this.svg.setAttribute("width",wp);
+		this.svg.setAttribute("height",hp);
+		this.svg.setAttribute("css","{width:'"+wp+"p‌​x',height:'"+hp+"px';}");
+		this.svg.innerHTML = "";
+	},
+	
+	transformCoords : function (obj){
+		var mX = 4096;
+		var mZ = 4096;
+		
+		obj.mapCoords[0] = obj.coords[0] * wp / mX;
+		obj.mapCoords[1] = obj.coords[1];
+		obj.mapCoords[2] = obj.coords[2] * hp / mZ;
+	},
+	
+	drawSvg : function(){
+		
+		this.svg.innerHTML = "";
+		
+		this.drawGrid();
+		this.drawAxis();
+		
+		this.transformCoords(center);
+		this.transformCoords(pilgrim);
+		this.transformCoords(userLocation);
+		
+		this.drawStar(center,6,'#7672E8');
+		this.drawStar(pilgrim,4,'orange');
+		
+		if(userLocation.enabled){
+			this.drawStar(userLocation,4,'#36AC3A');
+			this.drawTravel(userLocation.mapCoords[0],userLocation.mapCoords[2],pilgrim.mapCoords[0],pilgrim.mapCoords[2]);
+		}
+	},
+	
+	drawTravel : function(x1,y1,x2,y2){
+		//this.drawArrow(x1,y1,x2,y2, '5,5');		
+	},
+	
+	drawGrid : function(){
+		var step = 30;
+		var localHtml = "";
+		
+		var color = 153; // 0x99
+		
+		var template = '<line x1="{{x1}}" y1="{{y1}}" x2="{{x2}}" y2="{{y2}}" style="stroke:rgb({{color}},{{color}},{{color}});stroke-width:{{width}}" />';
+		
+		for(var i = step;i<wp;i+=step){
+			localHtml+=Mustache.render(template, {x1: i, y1: 0, x2 : i, y2: hp, color: color, width: 1});
+		}
+		for(var i = step;i<hp;i+=step){
+			localHtml+=Mustache.render(template, {x1: 0, y1: i, x2 : wp, y2: i, color: color, width: 1});
+		}
+		
+		this.svg.innerHTML+=localHtml;
+	},
+	
+	drawStar : function(obj,size, fillStyle){
+		var centerX = obj.mapCoords[0];
+		var centerZ = obj.mapCoords[2];
+		
+		var template = '<circle cx="{{centerX}}" cy="{{centerZ}}" r="{{size}}" stroke="{{fillStyle}}" stroke-width="1" fill="{{fillStyle}}" />';
+		this.svg.innerHTML+=Mustache.render(template, {centerX: centerX, centerZ: centerZ, size:size, fillStyle: fillStyle});
+	},
+	drawText : function(text, x,y) {
+		var color = "white";
+		var template = '<text x="{{x}}" y="{{y}}" fill="{{color}}">{{text}}</text>';
+		this.svg.innerHTML+=Mustache.render(template, {x: x, y: y, color: color, text : text});
+	},
+	
+	drawArrow : function(x1,y1,x2,y2, stroke){
+		var color = 255;
+		var templateLine = '<line x1="{{x1}}" y1="{{y1}}" x2="{{x2}}" y2="{{y2}}" style="stroke:rgb({{color}},{{color}},{{color}}); stroke-width:{{width}};" {{{strokedash}}} />';
+		var templatePath = '<path d="{{text}}" fill="white" />';
+		
+		var localHtml = "";
+		
+		if (stroke!=undefined && stroke!=null){
+			stroke = 'stroke-dasharray="'+stroke+'"';
+		}else{
+			stroke="";
+		}
+		
+		localHtml+=Mustache.render(templateLine, {x1: x1, y1: y1, x2 : x2, y2: y2, color: color, width: 1, strokedash:stroke });
+		
+		var headlen = 10;   
+		var angle = Math.atan2(y2-y1,x2-x1);
+		
+		var pathText ="";
+		pathText += Mustache.render("M{{a}} {{b}} ", {a: x2,b:y2});
+		pathText += Mustache.render("L{{a}} {{b}} ", {a: x2-headlen*Math.cos(angle-Math.PI/6),b: y2-headlen*Math.sin(angle-Math.PI/6) });
+		pathText += Mustache.render("L{{a}} {{b}} ", {a: x2-headlen*Math.cos(angle+Math.PI/6),b: y2-headlen*Math.sin(angle+Math.PI/6) });
+		pathText+= " Z";
+    
+		localHtml+=Mustache.render(templatePath, {text: pathText});
+
+		this.svg.innerHTML+=localHtml;
+
+	},
+	
+	drawAxis : function (){
+		this.drawText("X",140,35);
+		this.drawArrow(10,10,150,10);
+		this.drawText("Z",18,150);
+		this.drawArrow(10,10,10,150);
+	}
+	
+};
+
+
+var canvasHelper = {
+
+	init: function(domId){
+		var canvas = document.getElementById("plotc");
+		var parentPercent = 0.9;
+		legendPercent = 0.0;
+		
+		var parent = document.getElementById("plotparent");
+		wp = parent.getBoundingClientRect().width * parentPercent;
+		hp = wp * (document.documentElement.clientHeight*1.0/document.documentElement.clientWidth); 
+
+		realCanvasWidth = wp;
+		realCanvasHeight = hp;
+		
+		canvas.setAttribute("width",wp);
+		canvas.setAttribute("height",hp);
+		canvas.setAttribute("css","{width:'"+wp+"p‌​x',height:'"+hp+"px';}");
+
+		canvasWidth = wp;
+		canvasHeight = hp;
+		ctx = canvas.getContext("2d");	 	
+		
+	},
+
+	drawGrid : function (){
+		ctx.strokeStyle = '#999999';
+		var step = 30;
+		
+		for(var i = 0;i<canvasWidth;i+=step){
+			ctx.beginPath();
+			ctx.moveTo(i,0);
+			ctx.lineTo(i,canvasHeight);
+			ctx.stroke();
+		}
+		for(var i = 0;i<canvasHeight;i+=step){
+			ctx.beginPath();
+			ctx.moveTo(0,i);
+			ctx.lineTo(canvasWidth,i);
+			ctx.stroke();
+		}
+	},
+	
+	drawDot : function (x,z,size,fillStyle){
+		var radius = size;
+		
 		ctx.beginPath();
-		ctx.moveTo(i,0);
-		ctx.lineTo(i,canvasHeight);
+		ctx.arc(x, z, radius, 0, 2 * Math.PI, false);
+		ctx.fillStyle = fillStyle;
+		ctx.strokeStyle = fillStyle;
+		ctx.fill();
+	},
+	
+	drawStar : function (x,z, size, fillStyle){
+		var zX = 0;
+		var zZ = 0;
+		var mX = 4096;
+		var mZ = 4096;
+		
+		var centerX = x * canvasWidth / mX;
+		var centerZ = z * canvasHeight / mZ;
+		this.drawDot(centerX,centerZ,size,fillStyle);
+	},
+	
+	drawText : function (text, x,y){
+		ctx.font="15px Georgia";
+		ctx.fillText(text,x,y);
+	},
+	
+	drawArrow : function (fromx, fromy, tox, toy){
+		var headlen = 10;   // length of head in pixels
+		var angle = Math.atan2(toy-fromy,tox-fromx);
+		ctx.moveTo(fromx, fromy);
+		ctx.lineTo(tox, toy);
 		ctx.stroke();
-	}
-	for(var i = 0;i<canvasHeight;i+=step){
-		ctx.beginPath();
-		ctx.moveTo(0,i);
-		ctx.lineTo(canvasWidth,i);
+		ctx.moveTo(tox, toy);
+		ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
 		ctx.stroke();
+		ctx.moveTo(tox, toy);
+		ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
+		ctx.stroke();
+	},
+	
+	drawAxis : function (){
+		ctx.fillStyle = '#ffffff';
+		ctx.strokeStyle = '#ffffff';
+		this.drawText("X",140,30);
+		this.drawArrow(10,10,150,10);
+		this.drawText("Z",18,150);
+		this.drawArrow(10,10,10,150);
+	},
+	
+	drawCanvas: function (){
+		ctx.clearRect(0, 0, realCanvasWidth, realCanvasHeight);
+		this.drawGrid();
+		this.drawAxis();
+		this.drawStar(center.coords[0], center.coords[2],6,'#7672E8');
+		this.drawStar(pilgrim.coords[0], pilgrim.coords[2],4,'orange');
+		
+		if(userLocation.enabled){
+			this.drawStar(userLocation.coords[0], userLocation.coords[2],4,'#36AC3A');
+		}
+	
 	}
-}
-
-function drawDot(x,z,size,fillStyle){
-	var radius = size;
-	
-	ctx.beginPath();
-    ctx.arc(x, z, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = fillStyle;
-	ctx.strokeStyle = fillStyle;
-    ctx.fill();
-}
-
-function drawStar(x,z, size, fillStyle){
-
-	var zX = 0;
-	var zZ = 0;
-	var mX = 4096;
-	var mZ = 4096;
-	
-	var centerX = x * canvasWidth / mX;
-	var centerZ = z * canvasHeight / mZ;
-	drawDot(centerX,centerZ,size,fillStyle);
 	
 }
 
-function drawText(text, x,y){
-	ctx.font="15px Georgia";
-	ctx.fillText(text,x,y);
-}
 
-function drawArrow(fromx, fromy, tox, toy){
-    var headlen = 10;   // length of head in pixels
-    var angle = Math.atan2(toy-fromy,tox-fromx);
-    ctx.moveTo(fromx, fromy);
-    ctx.lineTo(tox, toy);
-	ctx.stroke();
-    ctx.moveTo(tox, toy);
-	ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
-	ctx.stroke();
-	ctx.moveTo(tox, toy);
-    ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
-	ctx.stroke();
-}
-
-function drawAxis(){
-	
-	ctx.fillStyle = '#ffffff';
-	ctx.strokeStyle = '#ffffff';
-	drawText("X",140,30);
-	drawArrow(10,10,150,10);
-	drawText("Z",18,150);
-	drawArrow(10,10,10,150);
-	
-}
-
-function drawLegend(){
-	ctx.rect(realCanvasWidth*(1-legendPercent),0,realCanvasWidth*(1-legendPercent),realCanvasHeight);
-	ctx.strokeStyle = '#777777';
-	
-	var step = 30;
-	var offset = 15;
-
-	drawDot(realCanvasWidth*(1-legendPercent)+15, offset,6,'#7672E8');
-	drawText("Center",realCanvasWidth*(1-legendPercent)+25, offset+5);offset+=step;
-	drawDot(realCanvasWidth*(1-legendPercent)+15, offset,4,'orange');
-	drawText("Pilgrim",realCanvasWidth*(1-legendPercent)+25, offset+5);offset+=step;
-	
-	if(userLocation.enabled){
-		drawDot(realCanvasWidth*(1-legendPercent)+15, offset,4,'#36AC3A');
-		drawText("User",realCanvasWidth*(1-legendPercent)+25, offset+5);offset+=step;
-	}
-}
-
-function drawCanvas(){
-	ctx.clearRect(0, 0, realCanvasWidth, realCanvasHeight);
-	drawGrid();
-	drawAxis();
-	drawStar(center.coords[0], center.coords[2],6,'#7672E8');
-	drawStar(pilgrim.coords[0], pilgrim.coords[2],4,'orange');
-	
-	if(userLocation.enabled){
-		drawStar(userLocation.coords[0], userLocation.coords[2],4,'#36AC3A');
-	}
-	drawLegend();
-}
 
 function generateMap(){
-	var canvas = document.getElementById("plotc");
-	
-	var parentPercent = 0.7;
-	legendPercent = 0.2;
-	
-	var parent = document.getElementById("plotparent");
-	var wp = parent.getBoundingClientRect().width*parentPercent;
-	var hp = wp*(1-legendPercent);
 
-	realCanvasWidth = wp;
-	realCanvasHeight = hp;
+	// Draw using canvas
+	//canvasHelper.init("plotc");
+	//canvasHelper.drawCanvas();
 	
-	canvas.setAttribute("width",wp);
-	canvas.setAttribute("height",hp);
-	canvas.setAttribute("css","{width:'"+wp+"p‌​x',height:'"+hp+"px';}");
-
-	canvasWidth = hp;
-	canvasHeight = hp;
-	ctx = canvas.getContext("2d");	 	
 	
-	drawCanvas();
+	// Draw on SVG
+	svgHelper.init("galsvg",wp,hp);
+	svgHelper.drawSvg();
+	
 }
 
 function hideMessages(){
 	var elementA = document.getElementById("errorMessage");
 	var elementB = document.getElementById("locationInfo");
 	
-	elementA.className = "hidden";
+	elementA.className = elementA.className + " hidden";
 	elementB.className = "hidden";
 }
 
 function showErrorMessage(text){
 	var element = document.getElementById("errorMessage");
-	element.innerHTML = text;
-	element.className = "visible button-error pure-button";
+	element.className = "card indigo";
+	var elementText = document.getElementById("errorMessageText");
+	elementText.innerHTML = text;
 }
 
 function calculateDistance(x0,y0,z0,x1,y1,z1){
@@ -192,7 +322,9 @@ function showLocationInfo(x,y,z){
 	setValue("cdistance",calculateDistance(x,y,z,center.coords[0],center.coords[1],center.coords[2]) +" ly");
 	setValue("pdistance",calculateDistance(x,y,z,pilgrim.coords[0],pilgrim.coords[1],pilgrim.coords[2]) +" ly");
 	setValue("pjumps",Math.ceil(calculateDistance(x,y,z,pilgrim.coords[0],pilgrim.coords[1],pilgrim.coords[2])/400.0));
-	drawCanvas();
+
+	//canvasHelper.drawCanvas();
+	svgHelper.drawSvg();
 	
 }
 
